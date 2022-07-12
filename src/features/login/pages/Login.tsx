63786352cb6fc65d4f7f71de3277c7187d2cf4f3
jmora-dev <dam2mora@gmail.com>
2022-07-12 19:06:29 +0200

    Add login custom hooks

--- src/features/login/pages/Login.tsx
+++ src/features/login/pages/Login.tsx
@@ -1,30 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { RootState } from "../../../redux/store";
-import { loginActionsCreators } from "../reducer/loginActionsCreators";
-import { api } from "../services/firebaseApi";
+// import { useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
 
 export default function Login() {
-  const dispatch = useDispatch();
-  const { token } = useSelector((state: RootState) => state.login);
-  const navigate = useNavigate();
+  const { signIn } = useLogin();
+  // const navigate = useNavigate();
 
-  const signIn = () => {
-    api.login("dam2mora@gmail.com", "123456").then((res) => {
-      dispatch(loginActionsCreators.login(res));
-    });
-  };
-
-  const test = () => {
-    fetch(process.env.REACT_APP_API_URL + `/exercises.json?auth=${token}`)
-      .then((res) => res.json())
-      .then((res) => console.log(res));
+  const onSignIn = () => {
+    signIn("dam2mora@gmail.com", "123456");
   };
 
   return (
     <>
-      <button onClick={signIn}>signIn</button>
-      <button onClick={test}></button>
+      <button onClick={onSignIn}>signIn</button>
     </>
   );
 }
