f7bfe429dfe3bc76b60717de60b32c0dd30f55dd
jmora-dev <dam2mora@gmail.com>
2022-07-13 19:10:17 +0200

    Add appllication config routes

---
+++ src/config/routes.ts
@@ -0,0 +1,14 @@
+export const PRIVATE_SECTION = "/private";
+
+export const ROUTES = {
+  LOGIN: "/",
+  LOGOUT: "/logout",
+  EXERCISES: "/exercises",
+  EXERCISE_DETAIL: "/exercise/detail",
+  EXERCISE_ADD: "/exercise/add",
+  EXERCISE_UPDATE: "/exercise/update",
+  TRAININGS: "/trainings",
+  TRAININGS_DETAIL: "/trainings/detail",
+  TRAININGS_ADD: "/trainings/add",
+  TRAININGS_UPDATE: "/trainings/update",
+};
