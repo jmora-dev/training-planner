55667a41501d245b48f344ff300bfce27bfb8036
jmora-dev <dam2mora@gmail.com>
2022-07-23 09:35:00 +0200

    Add components exports

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,1 +1,4 @@
 export { default as Card } from "./Card/Card";
+export { default as Input } from "./Input/Input";
+export { default as Label } from "./Label/Label";
+export { default as Button } from "./Button/Button";
